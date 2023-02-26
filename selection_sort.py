4f6fd5f17859c2d4651a6b6740c7b44b6d173d44
Sienna Rodriguez <siennamrodriguez@gmail.com>
2023-02-26 20:04:31 +1100

    Add docstring for selection sort algorithm

--- selection_sort.py
+++ selection_sort.py
@@ -1,4 +1,24 @@
 def selection_sort(list):
+    '''
+    Selects the i-th smallest element and places at i-th position. 
+    This algorithm divides the array into 2 parts: sorted (left) and
+    unsorted (right) subarray. It selects the smallest element from the
+    unsorted subarray and places in the first position of that subarray
+    (ascending order). It repeatedly selects the next smallest element
+
+    Parameters:
+        nums: (int) array of items 
+    
+    Returns: 
+        nums: (int) array of items sorted in acending order
+
+    Time complexity:
+        Best case: O(N^2) and O(1) swaps
+        Worst case: O(N^2) - reversely sorted and inner loops makes max comparisons
+        Average case: O(N^2) 
+
+    Space complexity: O(1) auxiliary - in-place sort.
+    '''
     n = len(list)
 
     for i in range(n-1):
